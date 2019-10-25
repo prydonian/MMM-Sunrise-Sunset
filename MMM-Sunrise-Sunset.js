e11e3ea9ce3722ee2dcc4d38c7dcae018c7158a2
Prydonian Digital <mark@prydonian.digital>
2019-10-25 10:51:15 +0100

    layout

--- MMM-Sunrise-Sunset.js
+++ MMM-Sunrise-Sunset.js
@@ -2,11 +2,13 @@ Module.register("MMM-Sunrise-Sunset",{
 	defaults: {
 		latitude: "",
 		longitude: "",
+		layout: "inline",
 		updateInterval: 3600000
 	},
     start: function () {
 		Log.info("Starting module: " + this.name);
 		this.apiBase = "https://api.sunrise-sunset.org/json?lat="+this.config.latitude+"&lng="+this.config.longitude+"&date=today";
+		var layout = this.config.layout;
 		this.updateSun(this);
 		setInterval(function() {
 			this.updateSun(this);
@@ -26,8 +28,15 @@ Module.register("MMM-Sunrise-Sunset",{
     },
     updateSun: function() {
 	    var url = this.apiBase;
-	    $.getJSON( url, function( data ) {
-		    $('.sunrise').html('<i class="wi wi-sunrise"></i> '+data.results.sunrise + ' <i class="wi wi-sunset"></i> '+data.results.sunset + ' <i class="wi wi-day-sunny"></i> '+data.results.solar_noon);
-		});
+	    if(this.config.layout === "inline"){
+    	    $.getJSON( url, function( data ) {
+        		$('.sunrise').html('<i class="wi wi-sunrise"></i> '+data.results.sunrise + ' <i class="wi wi-sunset"></i> '+data.results.sunset + ' <i class="wi wi-day-sunny"></i> '+data.results.solar_noon);
+        	});
+    	}
+		if(this.config.layout === "list"){
+			$.getJSON( url, function( data ) {
+    		    $('.sunrise').html('<div><i class="wi wi-sunrise"></i> '+data.results.sunrise + '</div><div><i class="wi wi-sunset"></i> '+data.results.sunset + '</div><div><i class="wi wi-day-sunny"></i> '+data.results.solar_noon) + '</div>';
+    		});
+    	}
     }
 });
