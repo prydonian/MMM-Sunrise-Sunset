64d9eb9327dd24fd1f896bd6f19ca551c19d349a
Prydonian Digital <mark@prydonian.digital>
2019-10-26 11:37:01 +0100

    Update MMM-Sunrise-Sunset.js

--- MMM-Sunrise-Sunset.js
+++ MMM-Sunrise-Sunset.js
@@ -3,6 +3,7 @@ Module.register("MMM-Sunrise-Sunset",{
 		latitude: "",
 		longitude: "",
 		timezone: "Europe/London",
+		daylength: true,
 		layout: "inline",
 		updateInterval: 3600000
 	},
@@ -31,18 +32,32 @@ Module.register("MMM-Sunrise-Sunset",{
 	    var offset = this.config.timezone;
 	    if(this.config.layout === "inline"){
     	    $.getJSON( url, function( data ) {
-            	var sunrise = moment.tz(data.results.sunrise, offset);
-    	        var sunset = moment.tz(data.results.sunset, offset);
-    	        var noon = moment.tz(data.results.solar_noon, offset);
-        		$('.sunrise').html('<i class="wi wi-sunrise"></i>'+ sunrise.format("hh:mm:ss a") + ' | <i class="wi wi-sunset"></i>'+ sunset.format("hh:mm:ss a") + ' | <i class="wi wi-day-sunny"></i>'+ noon.format("hh:mm:ss a"));
+	    	    $('.sunrise').html('');
+        	    var sunrise = moment.tz(data.results.sunrise, offset);
+        	    var sunset = moment.tz(data.results.sunset, offset);
+        	    var noon = moment.tz(data.results.solar_noon, offset);
+            	var day_length = data.results.day_length;
+                $('.sunrise').append('<i class="wi wi-sunrise"></i>'+ sunrise.format("hh:mm:ss a"));
+                $('.sunrise').append(' | <i class="wi wi-sunset"></i>'+ sunset.format("hh:mm:ss a"));
+                $('.sunrise').append(' | <i class="wi wi-day-sunny"></i>'+ noon.format("hh:mm:ss a"));
+                if(this.config.daylength === "true") {
+                  $('.sunrise').append(' | <i class="wi wi-time-1"></i>'+ parseFloat(dat_length/120).toFixed(2));
+                }
         	});
     	}
 		if(this.config.layout === "list"){
 			$.getJSON( url, function( data ) {
+	    	    $('.sunrise').html('');
             	var sunrise = moment.tz(data.results.sunrise, offset);
     	        var sunset = moment.tz(data.results.sunset, offset);
     	        var noon = moment.tz(data.results.solar_noon, offset);
-    		    $('.sunrise').html('<div><i class="wi wi-sunrise"></i>'+ sunrise.format("hh:mm:ss a") + '</div><div><i class="wi wi-sunset"></i>'+ sunset.format("hh:mm:ss a") + '</div><div><i class="wi wi-day-sunny"></i>'+ noon.format("hh:mm:ss a")) + '</div>';
+    	        var day_length = data.results.daylength;
+    	        $('.sunrise').append('<div><i class="wi wi-sunrise"></i>'+ sunrise.format("hh:mm:ss a") + '</div>');
+                $('.sunrise').append('<div><i class="wi wi-sunset"></i>'+ sunset.format("hh:mm:ss a") + '</div>');
+                $('.sunrise').append('<div><i class="wi wi-day-sunny"></i>'+ noon.format("hh:mm:ss a") + '</div>');
+                if(this.config.daylength === "true") {
+                  $('.sunrise').append('<div><i class="wi wi-time-1"></i>'+ parseFloat(dat_length/120).toFixed(2) + '</div>');
+                }
     		});
     	}
     }
